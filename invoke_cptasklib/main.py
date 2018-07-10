b9e6fa963d4d11fc3a483ab67490bf251da47fc1
Chris Plock <>
2018-07-10 16:17:31 -0700

    use libvirt.ns

--- invoke_cptasklib/main.py
+++ invoke_cptasklib/main.py
@@ -1,7 +1,6 @@
 from invoke import Collection
 from invoke import Program
-import invoke_cptasklib.tasks.file_util as file_util
 import invoke_cptasklib.tasks.libvirt as libvirt
 
-program = Program(namespace=Collection.from_module(libvirt), version='0.1')
-
+#program = Program(namespace=Collection.from_module(libvirt), version='0.1')
+program = Program(namespace=libvirt.ns, version='0.1')
